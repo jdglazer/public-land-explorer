26fffb4cc1a4b44c64cc526fcdd1901d340221dd
Joshua Glazer <atrail2014@gmail.com>
2026-07-08 01:15:26 -0700

    preserve changes for map

--- src/app/(userviews)/landsmap.tsx
+++ src/app/(userviews)/landsmap.tsx
@@ -1,9 +1,35 @@
-import { Map } from "@maplibre/maplibre-react-native";
-import React from "react";
+import { Camera, LogManager, OfflineManager, Map, MapRef, PressEvent, PressEventWithFeatures} from "@maplibre/maplibre-react-native";
+import React, { useRef } from "react";
+import { NativeSyntheticEvent } from "react-native";
 
 const LandsMap = () => {
+  const mapRef = useRef<MapRef | null>(null);
+
+  const handleMapPress = async (e: NativeSyntheticEvent<PressEvent>
+      | NativeSyntheticEvent<PressEventWithFeatures>) => {
+    const [xPix, yPix] = e.nativeEvent.point;
+    const features = await (mapRef.current?.queryRenderedFeatures([xPix, yPix]));
+    console.log("Features at pressed point:", features);
+  };
+
+  LogManager.setLogLevel('verbose');
+  LogManager.onLog((event) => {
+    console.log(`[MapLibre Native Log] ${event.level}: ${event.message}`);
+    return true; // Return true to override default logging if needed
+  });
+
+  const setAmbientCacheSize = async () => {
+      await OfflineManager.setMaximumAmbientCacheSize(0);
+  }
+
+  setAmbientCacheSize();
+  
   return (
-    <Map mapStyle="http://192.168.1.58:8080/styles/basemap/style.json" />
+    <Map ref={mapRef}
+         mapStyle="http://192.168.1.58/styles/basemap/style.json"
+         onPress={handleMapPress}>
+      <Camera initialViewState={{zoom: 2,  center: [-110.75, 44.5]}}></Camera>
+    </Map>
   );
 };
 
